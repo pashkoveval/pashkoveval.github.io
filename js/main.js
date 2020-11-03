59380dfad4e28e4a9b40f532b42d1045c36edc9f
pashkoveval <61497581+pashkoveval@users.noreply.github.com>
2020-11-03 23:44:29 +0300

    new

--- js/main.js
+++ js/main.js
@@ -2,10 +2,19 @@ new Vue({
     el: '#app',
     data: {
         ru: true,
+        top: '',
+        left: '',
+        onHref: '',
+        outHref: '',
     },
     methods: {
-
+        
+        move($event) {
+            this.top = $event.pageY + 'px';
+            this.left = $event.pageX + 'px';
+        }
     }, 
+    
 
     
 });
